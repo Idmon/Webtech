99f13bee8aaad53bfcc653ee0deedda5ef08ec98
ThijsImmink <thijs_immink@live.nl>
2013-09-04 16:09:18 +0200

    test

--- src/idmon.java
+++ src/idmon.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class idmon
+ * Servlet implementation class idmon en thijs
  */
 @WebServlet("/idmon")
 public class idmon extends HttpServlet {
